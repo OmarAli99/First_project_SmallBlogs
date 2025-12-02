47e2618466c84d3701167bcce1fd4b6a72d2a716
Omar Ali <omar_ali@eigtec.net>
2025-12-02 14:12:24 +0200

    project - theme to views module

---
+++ resources/views/theme/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('theme.partial.head')
+<body>
+
+  @include('theme.partial.header')
+
+    @yield('content')
+
+  @include('theme.partial.footer')
+
+  @include('theme.partial.scripts')
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ThemeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::controller(ThemeController::class)-> name('theme.')->group(function()
+{
+    Route::get('/','index')->name('index');
+    Route::get('/category','category')->name('category');
+    Route::get('/contact','contact')->name('contact');
+    Route::get('/single_blog','singleblog')->name('singleblog');
+    Route::get('/login','login')->name('login');
+    Route::get('/register','register')->name('register');
+
+
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
